166b583bb96755d1d6bc0fcdac35145849813803
devansh-twinspark <devansh.mishra@twinspark.co>
2023-11-29 14:50:40 +0530

    update

--- wp-content/themes/understrap/functions.php
+++ wp-content/themes/understrap/functions.php
@@ -43,3 +43,9 @@ if ( class_exists( 'Jetpack' ) ) {
 foreach ( $understrap_includes as $file ) {
 	require_once get_theme_file_path( $understrap_inc_dir . $file );
 }
+
+// Disables the block editor from managing widgets in the Gutenberg plugin.
+add_filter( 'gutenberg_use_widgets_block_editor', '__return_false', 100 );
+
+// Disables the block editor from managing widgets. renamed from wp_use_widgets_block_editor
+add_filter( 'use_widgets_block_editor', '__return_false' );
